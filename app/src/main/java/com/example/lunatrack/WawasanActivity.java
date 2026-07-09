b73cf93d900772c0cda024064fadfe0d75a6e6c5
HP <2511500084@mahasiswa.atmaluhur.ac.id>
2026-07-09 11:46:57 +0700

    simpan semua file

---
+++ app/src/main/java/com/example/lunatrack/WawasanActivity.java
@@ -0,0 +1,14 @@
+package com.example.lunatrack;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WawasanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wawasan);
+    }
+}
